12816360402272fb8fe262053d392581ba4721bf
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-19 18:00:09 +0200

    base for 03 ui-routes

---
+++ 04-ui-route/app.js
@@ -0,0 +1,21 @@
+angular.module("app", [])
+
+.config(function($routeProvider)
+{
+
+})
+
+.controller("homeCtrl", function()
+{
+
+})
+
+.controller("loginCtrl", function()
+{
+
+})
+
+.controller("registerCtrl", function()
+{
+
+})
