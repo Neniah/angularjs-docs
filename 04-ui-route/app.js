e6b3dba30aec440af70eb3dc2d5842355fe487f1
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-19 18:18:46 +0200

    home, login, users, editUsers

--- 04-ui-route/app.js
+++ 04-ui-route/app.js
@@ -1,7 +1,36 @@
-angular.module("app", [])
+angular.module("app", ['ui.router'])
 
-.config(function($routeProvider)
+.config(function($stateProvider, $urlRouterProvider)
 {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'templates/home.html',
+      controller: 'homeCtrl'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'loginCtrl'
+    })
+    .state('users', {
+      url: '/users',
+      templateUrl: 'templates/ursers.html',
+      controller: 'usersCtrl'
+    })
+    .state('users', {
+      url: '/users',
+      templateUrl: 'templates/ursers.html',
+      controller: 'usersCtrl'
+    })
+    .state('editUsers', {
+      url: '/users/:id/edit',
+      templateUrl: 'templates/ursers.edit.html',
+      controller: function($scope, $stateParams){
+        $scope.userId = $stateParams.id;
+      }
+    })
+
 
 })
 
