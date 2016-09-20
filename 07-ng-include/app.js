634f5d266ccb1a33ba7cdcd006e9a7c2799a693e
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-20 17:30:24 +0200

    including a template

--- 07-ng-include/app.js
+++ 07-ng-include/app.js
@@ -1,5 +1,14 @@
 angular.module('app', [])
 
 .controller('appCtrl', function($scope){
-
+  $scope.users = [
+    {
+      "name": "Peter",
+      "age": "29"
+    },
+    {
+      "name": "Thomas",
+      "age": "32"
+    }
+  ];
 })
