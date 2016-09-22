c1367730716cb554b170b338691947253e9641fb
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-22 17:01:50 +0200

    users

--- 08-filters/app.js
+++ 08-filters/app.js
@@ -1,5 +1,13 @@
 angular.module('app', [])
 
 .controller('appCtrl', function($scope){
-  
+  $scope.users = [
+    {name: 'Jhon', phone: '555-44-333', age: '43'},
+    {name: 'Mark', phone: '033-12-090', age: '43'},
+    {name: 'Paul', phone: '878-40-333', age: '43'},
+    {name: 'Kate', phone: '455-44-222', age: '43'},
+    {name: 'Peter', phone: '234-21-956', age: '43'},
+    {name: 'Loreen', phone: '678-12-250', age: '43'},
+
+  ];
 })
