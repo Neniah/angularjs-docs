e8f1c03c95c61d0ffd9fa137392c4d104483f34e
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-24 17:53:51 +0200

    ng-repeat and ng-show

--- 12-ng/app.js
+++ 12-ng/app.js
@@ -1,5 +1,7 @@
 angular.module('app', [])
 
-.controller('appCtrl', function($scope, Users){
-
+.controller('appCtrl', function($scope){
+  $scope.numbers = [1, 2];
+  $scope.bool = false;
+  $scope.option = 2;
 })
