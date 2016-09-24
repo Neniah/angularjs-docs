23fe24c03ef662c78c3b8d097db774acb97fc23a
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-24 18:34:06 +0200

    ng-hide

--- 12-ng/app.js
+++ 12-ng/app.js
@@ -1,7 +1,7 @@
 angular.module('app', [])
 
 .controller('appCtrl', function($scope){
-  $scope.numbers = [1, 2];
+  $scope.numbers = [1, 2, 5, 4, 8 ,7];
   $scope.bool = false;
   $scope.option = 2;
 })
