47c7b5f83b29008b04a813a9191de0645ab50641
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-24 17:39:48 +0200

    a service

--- 11-services/app.js
+++ 11-services/app.js
@@ -1,4 +1,42 @@
 angular.module('app', [])
 
-.controller('appCtrl', function($scope, navigatorInfo){
+.controller('appCtrl', function($scope, Users){
+  $scope.users = Users.usuarios();
+})
+
+.service("Users", function(){
+  this.usuarios = function(){
+    return [
+      {
+        id: 0,
+        name: "Hook",
+        age: 26
+      },
+      {
+        id: 1,
+        name: "Peter",
+        age: 32
+      },
+      {
+        id: 2,
+        name: "Samantha",
+        age: 27
+      },
+      {
+        id: 3,
+        name: "Jack",
+        age: 43
+      },
+      {
+        id: 4,
+        name: "Sussan",
+        age: 54
+      },
+      {
+        id: 5,
+        name: "Nate",
+        age: 33
+      }
+    ]
+  }
 })
