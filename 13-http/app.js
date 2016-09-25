97edc6ee8e1d978ad33c7fba394c371484639fa3
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-25 13:14:42 +0200

    a factory for http request

--- 13-http/app.js
+++ 13-http/app.js
@@ -1,5 +1,16 @@
 angular.module('app', [])
 
-.controller('appCtrl', function($scope){
+.controller('appCtrl', function($scope, UsersFactory){
 
+})
+
+.factory("UsersFactory", function($http){
+  return {
+    get: function(){
+      return $http({
+        url: "users.json",
+        method: "GET"
+      })
+    }
+  }
 })
