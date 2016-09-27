06088a4b27040e8ead5d4782dfe3169c5fd51f4d
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-27 12:34:15 +0200

    config httpProvider

--- 14-$resource/app.js
+++ 14-$resource/app.js
@@ -1,4 +1,10 @@
-angular.module('app', [])
+angular.module('app', ["ngResource"])
+
+.config(function($httpProvider){
+  delete $httpProvider.defaults.headers.common["X-Requested-With"];
+  
+})
 
 .controller('appCtrl', function($scope){
+
 })
