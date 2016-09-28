ace090d46560c6f6b434932b5bbd3f751ba4a4e9
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-28 09:39:34 +0200

    factory UsersFactory

--- 14-$resource/app.js
+++ 14-$resource/app.js
@@ -5,6 +5,10 @@ angular.module('app', ["ngResource"])
   $httpProvider.defaults.headers.post["Content-type"] = "application/x-www-form-urlencoded; charset=UTF-8";
 })
 
-.controller('appCtrl', function($scope){
+.controller('appCtrl', function($scope, UsersFactory){
 
+})
+
+.factory("UsersFactory", function($resource){
+  return $resource("http://localhost/php/slimrest/books/:id")
 })
