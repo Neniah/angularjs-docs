7d4411bc28333c975ebfbf80f12153cdbd036313
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-28 09:37:26 +0200

    defaults

--- 14-$resource/app.js
+++ 14-$resource/app.js
@@ -2,7 +2,7 @@ angular.module('app', ["ngResource"])
 
 .config(function($httpProvider){
   delete $httpProvider.defaults.headers.common["X-Requested-With"];
-  
+  $httpProvider.defaults.headers.post["Content-type"] = "application/x-www-form-urlencoded; charset=UTF-8";
 })
 
 .controller('appCtrl', function($scope){
