9e654bc001ee06b3619434edeea96173547f3b37
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-09-29 12:23:39 +0200

    building a form

--- 18-ng-message/app.js
+++ 18-ng-message/app.js
@@ -1,6 +1,8 @@
-angular.module('app', [])
+angular.module('app', ["ngMessages"])
 
 .controller('appCtrl', function($scope){
-
+  $scope.register = function(user){
+    console.log(JSON.stringify(user));
+  }
 
 })
