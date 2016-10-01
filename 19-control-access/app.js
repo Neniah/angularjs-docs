c8402e2ad8e52afd12610e9e6be1567e5dafa947
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-10-01 19:18:27 +0200

    another state for dashboard

--- 19-control-access/app.js
+++ 19-control-access/app.js
@@ -4,7 +4,13 @@ angular.module("app", ["ui.router", "ngMessages", "ngStorage"])
   $stateProvider
     .state("app", {
       abstract: true,
-
+      templateUrl: "templates/navigation.html",
+      controller: navigationCtrl
+    })
+    .state("app.dashboard",{
+      url: "/dashboard",
+      templateUrl: "templates/dashboard.html",
+      controller: "dashboardCtrl"
     })
   $urlRouteProvider.otherwise("/");
 })
