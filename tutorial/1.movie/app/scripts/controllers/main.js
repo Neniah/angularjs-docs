cb12e3665cae6dee65985371fa479cf5131d5255
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-11-29 11:29:10 +0100

    MainController

--- tutorial/1.movie/app/scripts/controllers/main.js
+++ tutorial/1.movie/app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the movieApp
  */
 angular.module('movieApp')
-  .controller('MainCtrl', function () {
+  .controller('MainController', function () {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
