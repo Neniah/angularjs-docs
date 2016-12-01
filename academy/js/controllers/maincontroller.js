1ff64eea3a30d9ae6a93e4b729ed0f197607d9ea
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-12-01 18:24:12 +0100

    date

--- academy/js/controllers/maincontroller.js
+++ academy/js/controllers/maincontroller.js
@@ -4,7 +4,8 @@ app.controller('MainController', [
     $scope.promo = 'The Books of this month.';
     $scope.product = {
       name: 'The Eight',
-      price: 19
+      price: 19,
+      pubdate: new Date('2014', '03', '08')
     };
   }
 ]);
