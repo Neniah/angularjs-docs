d41c13719c40ffd91ade1287183823f6eef0991d
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-12-02 16:58:38 +0100

    products

--- academy/js/controllers/maincontroller.js
+++ academy/js/controllers/maincontroller.js
@@ -2,10 +2,19 @@ app.controller('MainController', [
   '$scope', function($scope){
     $scope.title = 'English Books';
     $scope.promo = 'The Books of this month.';
-    $scope.product = {
-      name: 'The Eight',
-      price: 19,
-      pubdate: new Date('2014', '03', '08')
-    };
+    $scope.products = [
+   {
+     name: 'The Eight',
+     price: 19,
+     pubdate: new Date('2014', '03', '08'),
+     cover: 'img/the8.jpg'
+   },
+   {
+     name: 'Program or be Programmed',
+     price: 8,
+     pubdate: new Date('2013', '08', '01'),
+     cover: 'img/pendulum.jpg'
+   }
+ ];
   }
 ]);
