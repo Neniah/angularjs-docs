fe6fa9b7706839f134739171d6cbb5448885123f
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-12-18 17:40:43 +0100

    Article class

--- book2/angular2_reddit/src/app/article/article.component.ts
+++ book2/angular2_reddit/src/app/article/article.component.ts
@@ -3,11 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  host: {
+    class: 'row'
+  }
 })
 export class ArticleComponent implements OnInit {
+  votes: number;
+  title: string;
+  link: string;
 
-  constructor() { }
+  constructor() {
+    this.title = 'Angular 2';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
+
+  voteUp(){
+    this.votes += 1;
+  }
+
+  voteDown(){
+    this.votes -= 1;
+  }
 
   ngOnInit() {
   }
