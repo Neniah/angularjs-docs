bca44fc4c475a7cfae81f2d9145cb8f7905efaab
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-12-23 10:27:42 +0100

    variables and types

--- angular2tutorial/tys/prueba.ts
+++ angular2tutorial/tys/prueba.ts
@@ -7,3 +7,9 @@ var resultado = helloWorld(name);
 
 var etiqueta = <HTMLElement>document.getElementById('container');
 etiqueta.innerHTML = resultado;
+
+// Variables and types
+var name:tring = "Jane Doe";
+var age:number = 55;
+var programmer:boolean = true;el
+var languages: Array<string> = ["PHP", "JS", "Perl", "Java", "Python", "C"];
