3e8d5950efa2cf7eb8b392971ad8e8386de69423
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-12-23 12:08:31 +0100

    functions and types

--- angular2tutorial/tys/prueba.js
+++ angular2tutorial/tys/prueba.js
@@ -1,7 +1,30 @@
-function helloWorld(ename) {
-    return "Hello World!! I'm " + ename;
+/*
+function helloWorld(ename){
+  return "Hello World!! I'm " + ename;
 }
+
 var name = "María Lobillo Santos";
 var resultado = helloWorld(name);
+*/
 var etiqueta = document.getElementById('container');
-etiqueta.innerHTML = resultado;
+//etiqueta.innerHTML = resultado;
+// Variables and types
+var fullname = "Jane Doe";
+var age = 55;
+var programmer = true;
+var languages = ["PHP", "JS", "Perl", "Java", "Python", "C"];
+etiqueta.innerHTML = fullname + " - " + age;
+// let and var
+var a = 7;
+var b = 12;
+if (a === 7) {
+    var a_1 = 4;
+    var b = 1;
+    console.log("IF Statement : " + a_1 + " - " + b);
+}
+console.log("Out of IF Statement : " + a + " - " + b);
+// Functions
+function getNumber(num) {
+    return num;
+}
+alert(getNumber(34));
--- angular2tutorial/tys/prueba.ts
+++ angular2tutorial/tys/prueba.ts
@@ -1,15 +1,39 @@
+/*
 function helloWorld(ename){
   return "Hello World!! I'm " + ename;
 }
 
 var name = "María Lobillo Santos";
 var resultado = helloWorld(name);
+*/
 
 var etiqueta = <HTMLElement>document.getElementById('container');
-etiqueta.innerHTML = resultado;
+//etiqueta.innerHTML = resultado;
 
 // Variables and types
-var name:tring = "Jane Doe";
+var fullname:string = "Jane Doe";
 var age:number = 55;
-var programmer:boolean = true;el
+var programmer:boolean = true;
 var languages: Array<string> = ["PHP", "JS", "Perl", "Java", "Python", "C"];
+
+etiqueta.innerHTML = fullname + " - " + age;
+
+// let and var
+var a = 7;
+var b = 12;
+
+if(a === 7){
+  let a = 4;
+  var b = 1;
+
+  console.log("IF Statement : " + a + " - " + b);
+}
+
+console.log("Out of IF Statement : " + a + " - " + b);
+
+// Functions
+function getNumber(num:number){
+  return num;
+}
+
+alert(getNumber(34));
