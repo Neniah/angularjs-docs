b400e56fc79f4e35659aab890e3de2720e447535
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-12-30 19:20:43 +0100

    adding the link for add-movie

--- angular2tutorial/angular-1/app/app.component.js
+++ angular2tutorial/angular-1/app/app.component.js
@@ -1,4 +1,4 @@
-System.register(["angular2/core", "./components/movies-list.component", "./components/movies-footer.component", "angular2/router", "./components/contact.component"], function(exports_1, context_1) {
+System.register(["angular2/core", "./components/movies-list.component", "./components/movies-footer.component", "angular2/router", "./components/contact.component", "./components/add-movie.component"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(["angular2/core", "./components/movies-list.component", "./compo
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, movies_list_component_1, movies_footer_component_1, router_1, contact_component_1;
+    var core_1, movies_list_component_1, movies_footer_component_1, router_1, contact_component_1, add_movie_component_1;
     var AppComponent;
     return {
         setters:[
@@ -28,6 +28,9 @@ System.register(["angular2/core", "./components/movies-list.component", "./compo
             },
             function (contact_component_1_1) {
                 contact_component_1 = contact_component_1_1;
+            },
+            function (add_movie_component_1_1) {
+                add_movie_component_1 = add_movie_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
@@ -38,11 +41,14 @@ System.register(["angular2/core", "./components/movies-list.component", "./compo
                     core_1.Component({
                         selector: "my-app",
                         templateUrl: "app/view/movies.html",
-                        directives: [movies_list_component_1.MoviesListComponent, movies_footer_component_1.MoviesFooterComponent, router_1.ROUTER_DIRECTIVES, contact_component_1.ContactComponent],
+                        directives: [movies_list_component_1.MoviesListComponent,
+                            movies_footer_component_1.MoviesFooterComponent, router_1.ROUTER_DIRECTIVES,
+                            contact_component_1.ContactComponent, add_movie_component_1.AddMovieComponent],
                     }),
                     router_1.RouteConfig([
                         { path: "/movies", name: "Movies", component: movies_list_component_1.MoviesListComponent, useAsDefault: true },
-                        { path: "/contact", name: "Contact", component: contact_component_1.ContactComponent }
+                        { path: "/contact", name: "Contact", component: contact_component_1.ContactComponent },
+                        { path: "/add-movie", name: "AddMovie", component: add_movie_component_1.AddMovieComponent }
                     ]), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- angular2tutorial/angular-1/app/app.component.ts
+++ angular2tutorial/angular-1/app/app.component.ts
@@ -3,16 +3,20 @@ import { MoviesListComponent } from "./components/movies-list.component"
 import { MoviesFooterComponent } from "./components/movies-footer.component"
 import { ROUTER_DIRECTIVES, RouteConfig, Router } from "angular2/router";
 import { ContactComponent } from "./components/contact.component"
+import { AddMovieComponent } from "./components/add-movie.component"
+
 
 @Component({
   selector: "my-app",
   templateUrl:"app/view/movies.html",
-  directives: [MoviesListComponent, MoviesFooterComponent, ROUTER_DIRECTIVES, ContactComponent],
+  directives: [MoviesListComponent,
+    MoviesFooterComponent, ROUTER_DIRECTIVES,
+    ContactComponent, AddMovieComponent],
 })
 @RouteConfig([
   {path: "/movies", name: "Movies", component: MoviesListComponent, useAsDefault: true},
-  {path: "/contact", name: "Contact", component: ContactComponent}
-
+  {path: "/contact", name: "Contact", component: ContactComponent},
+  {path: "/add-movie", name: "AddMovie", component: AddMovieComponent}
 ])
 export class AppComponent{
   public title:string = "Movies on Angular 2";
