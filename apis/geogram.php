0c4bac0d65bc0e2ba4047b26a3ff24b5cf15ca4b
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-15 11:37:58 +0100

    Lat and long

--- apis/geogram.php
+++ apis/geogram.php
@@ -1,5 +1,13 @@
 <?php
+if(!empty($_GET['location'])){
+  $maps_url = 'https://maps.googleapis.com/api/geocode/json?address=' . urlencode($_GET['location']);
 
+  $maps_json = file_get_contents($map_url);
+  $maps_array = json_decode($maps_json, true);
+
+  $lat = $maps_array['results'][0]['geometry']['location']['lat'];
+  $lng = $maps_array['results'][0]['geometry']['location']['lng'];
+}
 
 ?>
 <!DOCTYPE html>
