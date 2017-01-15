84ecb2cbfb85b33734ea7d8f5d61cc5f9686923c
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-15 13:44:04 +0100

    The form

--- apis/php/index.php
+++ apis/php/index.php
@@ -3,10 +3,16 @@
   <head>
     <meta charset="utf-8">
     <title>JSON and PHP</title>
-    <script type="text/javascript" src="jquery.js"></script>
-    <script type="text/javascript" src="app.js"></script>
   </head>
   <body>
 
+    <form class="" action="add.php" method="post">
+      <input type="text" name="first" > +
+      <input type="text" name="second" >
+      <input type="submit" value="Work this out">
+    </form>
+
+    <script type="text/javascript" src="jquery.js"></script>
+    <script type="text/javascript" src="app.js"></script>
   </body>
 </html>
