33133c2a5eabbb9270f8b1e8cd61b2125db8d994
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-16 17:42:44 +0100

    A list with title and year

--- apis/movies/index.php
+++ apis/movies/index.php
@@ -1,6 +1,10 @@
 <?php
   $jsondata = file_get_contents("movies.json");
   $json = json_decode($jsondata, true);
-
-  echo $json['movies'][0]['title'];
+  $output = '<ul>';
+  foreach($json['movies'] as $movie){
+    $output .= '<h4>' .$movie['title'].'</h4>';
+    $output .= '<li>'.$movie['year'].'</li>';
+  }
+  echo $output;
 ?>
