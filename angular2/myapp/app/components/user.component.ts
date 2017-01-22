7f41bb4a53f184e9a63f33fb75e4a43b2a20a306
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-22 18:33:19 +0100

    *ngFor on an array

--- angular2/myapp/app/components/user.component.ts
+++ angular2/myapp/app/components/user.component.ts
@@ -6,12 +6,19 @@ import { Component } from '@angular/core';
     Hello {{name}}</h1>
     <p>Email: {{email}}</p>
     <p>{{address.street}} {{address.city}}, {{address.state}}</p>
+    <h3>Hobbies</h3>
+    <ul>
+      <li *ngFor="let hobby of hobbies">
+        {{hobby}}
+      </li>
+    </ul>
     `,
 })
 export class UserComponent  {
   name:string;
   email:string;
   address: address;
+  hobbies: string[];
 
   constructor(){
     this.name = 'Jane Doe';
@@ -21,6 +28,7 @@ export class UserComponent  {
       city: 'Boston',
       state: 'MA'
     }
+    this.hobbies = ['Music', 'Movies', 'Sports'];
   }
 
 }
