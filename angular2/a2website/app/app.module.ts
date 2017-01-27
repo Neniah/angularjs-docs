0aa2ecdce43403680a7f7e412a205db70d369fbb
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-27 16:09:53 +0100

    Jumbotron component

--- angular2/a2website/app/app.module.ts
+++ angular2/a2website/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { JumbotronComponent } from './component/jumbotron/jumbotron.component';
+import { HomeComponent } from './component/pages/home.component';
+import { AboutComponent } from './component/pages/about.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, NavbarComponent, JumbotronComponent,
+                    AboutComponent, HomeComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
