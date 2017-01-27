ebfe0bd4204203d5030f4d31840f58642a519201
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-27 16:21:38 +0100

    Navbar component

--- angular2/a2website/app/app.module.ts
+++ angular2/a2website/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { JumbotronComponent } from './component/jumbotron/jumbotron.component';
-import { HomeComponent } from './component/pages/home.component';
-import { AboutComponent } from './component/pages/about.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { HomeComponent } from './components/pages/home.component';
+import { AboutComponent } from './components/pages/about.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
