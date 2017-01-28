33d0b30e78617a1cd619a4912ff7876736378d1a
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-28 18:28:47 +0100

    Service

--- angular2/githubsearch/app/app.component.ts
+++ angular2/githubsearch/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { ProfileComponent } from './components/profile.component';
+import { GithubService } from './services/github.service';
+
 
 @Component({
   selector: 'my-app',
   template: `<h1>Hello Angular 2</h1>
     <profile></profile>`,
+  providers: [GithubService]
 })
 export class AppComponent  { }
