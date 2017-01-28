a12e0e1c025819e4a6fd82800041a13e0bf8b80c
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-28 16:58:16 +0100

    Profile component

--- angular2/githubsearch/app/app.component.ts
+++ angular2/githubsearch/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { ProfileComponent } from './components/profile.component';
 
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
+  directives: [ProfileComponent]
 })
 export class AppComponent  { name = 'Angular'; }
--- angular2/githubsearch/app/app.module.ts
+++ angular2/githubsearch/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { ProfileComponent } from './components/profile.component'
 
 @NgModule({
   imports:      [ BrowserModule ],
