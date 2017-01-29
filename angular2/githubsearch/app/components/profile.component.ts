0be621ec551a37c3a249443a8d8eb301b0227941
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-29 18:28:01 +0100

    View profile

--- angular2/githubsearch/app/components/profile.component.ts
+++ angular2/githubsearch/app/components/profile.component.ts
@@ -8,10 +8,12 @@ import 'rxjs/add/operator/map';
   templateUrl: `profile.component.html`,
 })
 export class ProfileComponent  {
+  user[];
 
   constructor(private _githubService:GithubService){
     this._githubService.getUser().subscribe(user => {
-      console.log(user);
+      //console.log(user);
+      this.user = user;
     })
   }
 }
