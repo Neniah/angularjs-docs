9e0dd05d041e2056fe5fcbae4437cd9845ca05c6
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-01-31 19:13:55 +0100

    Imports and moving things

--- angular2/tour-of-heroes/app/hero-detail.component.ts
+++ angular2/tour-of-heroes/app/hero-detail.component.ts
@@ -2,8 +2,15 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'my-hero-detail',
+  template: `
+      <div *ngIf="hero">
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+  `,
+
 })
 
 export class HeroDetailComponent{
-  
+  hero: Hero;
 }
