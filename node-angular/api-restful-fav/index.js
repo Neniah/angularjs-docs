5af83e36cb5e0a3919cccd13ded9c6844e3c876e
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-04-24 18:39:21 +0200

    Optional params

--- node-angular/api-restful-fav/index.js
+++ node-angular/api-restful-fav/index.js
@@ -9,20 +9,19 @@ var port = process.env.PORT || 3678;
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.get('/test/:name?', function(req, res){
+app.get('/test/:name?', (req, res) => {
 
-  if(){
+  if(req.params.name){
     var name = req.params.name;
   }else {
     var name = "Without name";
   }
-  var name = req.params.name;
 
   res.status(200).send({
     data: [2,3,4],
     message: "Hello world with NodeJS and Express - " + name });
 });
 
-app.listen(port, function(){
+app.listen(port, () => {
   console.log(`API REST FAV on http://localhost:${port}`);
 });
