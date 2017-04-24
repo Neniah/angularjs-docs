244b8dec1e0c088dc6d2023e6344dfe627d60e3d
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-04-24 17:11:30 +0200

    Route by get

--- node-angular/api-restful-fav/index.js
+++ node-angular/api-restful-fav/index.js
@@ -9,6 +9,11 @@ var port = process.env.PORT || 3678;
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+app.get('/prueba', function(req, res){
+
+  res.status(200).send({data[2,3,4],message: "Hello world with NodeJS and Express" });
+});
+
 app.listen(port, function(){
   console.log(`API REST FAV on http://localhost:${port}`);
 });
