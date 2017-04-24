2178c8a03ab7d7458880018255f3a5b77928aa12
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-04-24 11:09:10 +0200

    operators

--- node-angular/calculator.js
+++ node-angular/calculator.js
@@ -2,6 +2,29 @@
 
 var args = process.argv.slice(2);
 
-var operator = "sum";
+var operator = args[1];
+var num1 = parseFloat(args[0]);
+var num2 = parseFloat(args[2]);
+
+var result = '\n Insert the arguments \n';
+
+if(args.length == 3){
+  switch(operation){
+    case 'sum'
+      result = 'Sum: ' + parseFloat(num1 + num2);
+      break;
+    case 'less'
+      result = 'Subtraction: ' + parseFloat(num1 - num2);
+      break;
+    case 'times'
+      result = 'Multiplication: ' + parseFloat(num1 * num2);
+      break;
+    case 'by'
+      result = 'Division: ' + parseFloat(num1 / num2);
+      break;
+
+
+  }
+}
 
 console.log(args);
