da426ec97824ad160666a67bb4d6045a5234d662
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-04-26 20:10:21 +0200

    Save method and get favourite

--- node-angular/api-restful-fav/controllers/favourite.js
+++ node-angular/api-restful-fav/controllers/favourite.js
@@ -13,6 +13,32 @@ function test(req, res){
     message: "Hello world with NodeJS and Express - " + name });
 }
 
+function getFavourite(req, res){
+  var favouriteId = req.params.id;
+
+  res.status(200).send({data: favouriteId});
+
+}
+
+function getFavourites(req, res){
+
+}
+
+function saveFavourite(req, res){
+  var params = req.body;
+
+  res.status(200).send({favourite: params});
+}
+
+function updateFavourite(req, res){
+
+}
+
+function deleteFavourite(req, res){
+
+}
+
+
 module.exports = {
   test
 }
--- node-angular/api-restful-fav/routes/favourite.js
+++ node-angular/api-restful-fav/routes/favourite.js
@@ -5,5 +5,9 @@ var FavouriteController = require('../controllers/favourite');
 var api = express.Router();
 
 api.get('/test/:name?', FavouriteController.test);
+api.get('/favourite/:id?', FavouriteController.getFavourite);
+api.post('/favourite', FavouriteController.saveFavourite);
+
+
 
 module.exports = api;
